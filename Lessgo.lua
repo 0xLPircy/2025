1ea42759f2c1b3609bdaeb0986bd041cbccd0843
0xLPircy <taveesha.agarwal@gmail.com>
2024-12-31 23:57:57 +0530

    Init 2025

---
+++ Lessgo.lua
@@ -0,0 +1,12 @@
+-- this is a ceremonial repo for 2025 first commit
+
+print("LESSSGOOOO 2025")
+print("Loved what i did with 2024, not the best but i learned and im content with till today.")
+print("And now im ready for the rest fo it.")
+print("Peace.")
+print("Happiness.")
+print("Love.")
+print("Health.")
+print("Wealth.")
+print("Power.")
+print("Balance")
